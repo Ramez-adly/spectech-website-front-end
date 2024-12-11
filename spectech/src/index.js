ef3dd4eabec0b1a7407aedbedbec617ef725fae9
Ramez-adly <RA2200314@tkh.edu.eg>
2024-12-11 18:55:09 +0200

    added some css to the register

--- spectech/src/index.js
+++ spectech/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import LoginForm from './components/loginForm';
+import register from './components/register';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <LoginForm/>    
+  <Register/>    
   </React.StrictMode>
 );
 
